64b1a0d4732f86b5551133d9abaf7ff8b33bd991
Masus <thomas_st@hotmail.com>
2020-07-23 13:27:58 +0200

    Initial Tasks layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-card>
+      <v-toolbar id="nav">
+        <v-toolbar-title>Hands-On Serverless</v-toolbar-title>
+        <v-spacer/>
+        <v-toolbar-title>
+          <router-link to="/">Tasks</router-link>|
+          <router-link to="/create">Create</router-link>
+        </v-toolbar-title>
+      </v-toolbar>
+    </v-card>
+
+    <router-view />
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 }
---
+++ src/views/Tasks.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    <v-row>
+      <v-col align="center">
+        <v-card width="80%">
+          <v-card-title >
+            <h1 class="primary--text">Tasks</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th class="text-left">
+                      <h2>Task Name</h2>
+                    </th>
+                    <th class="text-left">
+                      <h2>Description</h2>
+                    </th>
+                    <th class="text-left">
+                      <h2>Status</h2>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="task in tasks" :key="task.name">
+                    <td><v-img contain max-width="50" :src="task.img"></v-img></td>
+                    <td>{{ task.name }}</td>
+                    <td>{{ task.description }}</td>
+                    <td>{{ task.status }}</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import tasks from "@/assets/tasks.json"
+
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      tasks: []
+    }
+  },
+  mounted() {
+    this.tasks = this.getTasks()
+  },
+  methods: {
+    getTasks: function() {
+      return tasks
+    }
+  },
+};
+</script>
