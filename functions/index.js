c99f09e328962389bb6cfa888615e7e64fc772ec
Masus <thomas_st@hotmail.com>
2020-09-24 14:32:24 +0200

    Implemented generateThumbnail

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,13 @@ admin.initializeApp();
 const app = express();
 app.use(cors({ origin: true }));
 
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
 let tasksRef = admin.firestore().collection("TASKS")
+var bucketRef = admin.storage().bucket();
 
 app.get('/tasks', (req, res) => {  // Try getting these directly from frontend
     tasksRef.get().
@@ -29,4 +35,46 @@ app.delete('/tasks/:name', (req, res) => {
     res.send(`Task with name: [${req.params.name}] deleted`);
 });
 
+app.get('/thumbnails', (req, res) => {
+    bucketRef.getFiles().then(lst => res.json(lst))
+})
+
+async function processUpload(object) {
+    const fileBucket = object.bucket; // The Storage bucket that contains the file.
+    const filePath = object.name; // File path in the bucket.
+    const fileName = path.basename(filePath);
+    const contentType = object.contentType; // File content type.
+
+    if (!contentType.startsWith('image/')) {
+        return console.log('This is not an image.');
+    }
+
+    if (fileName.startsWith('thumb_')) {
+        return console.log('Already a Thumbnail.');
+    }
+
+    // Download file from bucket.
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    const metadata = {
+        contentType: contentType,
+    };
+    await bucket.file(filePath).download({ destination: tempFilePath });
+    console.log('Image downloaded locally to', tempFilePath);
+    // Generate a thumbnail using ImageMagick.
+    await spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
+    console.log('Thumbnail created at', tempFilePath);
+    // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
+    const thumbFileName = `thumb_${fileName}`;
+    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+    // Uploading the thumbnail.
+    await bucket.upload(tempFilePath, {
+        destination: thumbFilePath,
+        metadata: metadata,
+    });
+    // Once the thumbnail has been uploaded delete the local file to free up disk space.
+    return fs.unlinkSync(tempFilePath);
+}
+
 exports.api = functions.region("europe-west1").https.onRequest(app);
+exports.generateThumbnail = functions.region("europe-west1").storage.object().onFinalize(async (object) => processUpload(object))
