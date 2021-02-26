ecb8522c7d807f47b48df9ef7bd5b4b46a81c0c3
MZU <manuela.zueger@ipt.ch>
2021-02-26 14:13:52 +0100

    add get and post methods

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 
 const admin = require('firebase-admin');
 admin.initializeApp();
-let tasksRef = admin.firestore().collection("TASKS")
+let tasksRef = admin.firestore().collection('TASKS')
 
 const express = require('express');
 const cors = require('cors');
@@ -10,11 +10,32 @@ const cors = require('cors');
 const app = express();
 app.use(cors({ origin: true }));
 
-app.get('/tasks', (req, res) => { 
-    res.set({ 'Access-Control-Allow-Origin': '*' });
-    res.send("[{\"description\":\"Mi first task\",\"status\":\"ToDo\",\"name\":\"Step 1\"},{\"status\":\"ToDo\",\"description\":\"My second task\",\"name\":\"Step 2\"}]");
+app.get('/tasks', (req, res) => {
+    const snapshot = tasksRef.get();
+    snapshot.then(result => {
+        var resultList = [];
+        result.forEach((doc) => {
+            resultList.push(doc.data());
+        });
+        
+        console.log("the task list which we want to return:");
+        console.log(resultList);
+        
+        res.set({ 'Access-Control-Allow-Origin': '*' });
+        res.send(resultList);
+    })
 })
-//app.post('/tasks', (req, res) => { /*TODO: ...*/ })
+
+app.post('/tasks', (req, res) => { 
+    tasksRef.add(req.body);
+      
+    console.log('Added task:');
+    console.log(req.body);
+
+    res.set({ 'Access-Control-Allow-Origin': '*' });
+    res.sendStatus(200);
+})
+
 //app.delete('/tasks/:name', (req, res) => { /*TODO: ...*/ })
  
 exports.api = functions.region("europe-west1").https.onRequest(app);
